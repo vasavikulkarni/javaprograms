ed9028117ae54c92324f99afd12fd43994e69bbb
vasavikulkarni <43089960+vasavikulkarni@users.noreply.github.com>
2018-09-08 08:34:14 -0700

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,21 @@
+class  Pattern
+{
+	public static void main(String[]args)
+	{
+	char ch ='A';
+	{
+		  for ( int i=1;i<=5; i++)
+	 {
+		 for( int j=1;j<=5;j++)
+		 
+		 {
+		System.out.print(ch);
+		 
+		 
+		 }
+		 System.out.println();
+		 ch++;
+	 }
+	}
+}
+}
---
+++ Pattern4.java
@@ -0,0 +1,100 @@
+class  Pattern4
+{
+	public static void main(String[]args)
+
+	{
+		char c=A;
+		for(int i=1;i<=5;i++)
+		{
+				System.out.print('c');
+				
+
+			}
+			System.out.println();
+			c++;
+		}
+	}
+}
+
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
++
+
+
+
+
+
+				{
+
+			System.out.print(" ");
+			else
+		System.out.print(j);
+				}
+		System.out.println();
+
+
+}		
+}			
+		
+		
+	
